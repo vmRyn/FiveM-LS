1128ba1068f4ba7672c95d9acf11cc6a6fd38029
Ryn <31045220+vmRyn@users.noreply.github.com>
2021-03-24 04:40:07 +0000

    Revert "Formatting"
    This reverts commit a46872b399b749ecfe04ce0287ea28e5807f18d4.

--- __resource.lua
+++ __resource.lua
@@ -1,14 +1,15 @@
 
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
 loadscreen 'index.html'
+
+
 files {
     'index.html',
-    'css/config.css',
     'css/stylesheet.css',
     'js/config.js',
     'js/app.js',
-    'js/fullscreen-video.js',
-    'js/snowflake-effect.js',
+    'css/config.css',
     'images/B1.png',
     'images/bg0.png', 
     'images/bg1.png', 
@@ -18,9 +19,9 @@ files {
     'images/bg5.png', 
     'images/bg6.png', 
     'images/bg7.png',
+    'images/logo.png',
     'images/EL1.png',
     'images/EL2.png',
     'images/EL3.png',
-    'images/EL4.png',
-    'images/logo.png'
+    'images/EL4.png'
 }
